cf3a6a5e8119717c44d064b66b879a5312bd24ba
Jack Anderer-McClelland <jack.edward03@gmail.com>
2020-07-23 12:50:59 -0700

    comment typo fixes

--- Resilio Connect API/JSMonitor/utils.js
+++ Resilio Connect API/JSMonitor/utils.js
@@ -6,8 +6,8 @@ module.exports = {
 function findArrayDiff(a1, a2) {
     var diff = [];
 
-    diff = a1.filter(x => !a2.includes(x))           // filters the elements from a1 that are not included a2 into an array.
-           .concat(a2.filter(x => !a1.includes(x))); // filters the elements from a2 that are not in a1 into an array
+    diff = a1.filter(x => !a2.includes(x))           // filters the elements from a1 that are not included in a2 into an array.
+           .concat(a2.filter(x => !a1.includes(x))); // filters the elements from a2 that are not included in a1 into an array
                                                      // using the concat() method, both arrays are taking and merged into a new array
     
     return diff;
