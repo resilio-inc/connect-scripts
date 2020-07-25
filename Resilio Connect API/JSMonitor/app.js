f6eb69f70af648dd23783dd7db0637a569a8eaa8
Jack Anderer-McClelland <jack.edward03@gmail.com>
2020-07-25 16:03:25 -0700

    task 9 part 3 fixes

--- Resilio Connect API/JSMonitor/app.js
+++ Resilio Connect API/JSMonitor/app.js
@@ -9,38 +9,39 @@ const { findArrayDiff } = require('./utils');
 
 //const { getAgentName, isAgentOnline, getAgentJobList } = require('./agents');
 
-setAgentProperty(5, "name", "abc");
-setAgentProperty(17, "name", "xyz");
-console.log("the name of job 5 is: " + getAgentProperty(5, "name"));
+//setAgentProperty(5, "name", "abc");
+//setAgentProperty(17, "name", "xyz");
+//console.log("the name of job 5 is: " + getAgentProperty(5, "name"));
 
-setJobProperty(5, "agents", [1,7,99,2,105]);
-setJobProperty(17, "agents", [7,18,42,55]);
-setJobProperty(17, "name", "Sales and Marketing Folder");
-console.log("the agents in job 5 are: " + getJobProperty(5, "agents"));
-console.log("the name of job 17 is: " + getJobProperty(17, "name"));
+//setJobProperty(5, "agents", [1,7,99,2,105]);
+//setJobProperty(17, "agents", [7,18,42,55]);
+//setJobProperty(17, "name", "Sales and Marketing Folder");
+//console.log("the agents in job 5 are: " + getJobProperty(5, "agents"));
+//console.log("the name of job 17 is: " + getJobProperty(17, "name"));
 
 initializeMCParams("demo29.resilio.com", 8443, "6BZK5YQ6ER72NWP2GB7MYKEGA2AQZUXCCEVU7G7H4JTDGLDRNPMA");
-console.log("some fake api response: " + getAPIRequest("/api/v2/jobs"));
+//console.log("some fake api response: " + getAPIRequest("/api/v2/jobs"));
 
 
 updateAgentList();
 
-setTimeout(function(){console.log (getAgentProperty(1, "name"));}, 3000);
-setTimeout(function(){console.log (getAgentProperty(1, "status"));}, 3000);
+//setTimeout(function(){console.log (getAgentProperty(1, "name"));}, 3000);
+//setTimeout(function(){console.log (getAgentProperty(1, "status"));}, 3000);
 
-updateJobsPerAgent();
+//updateJobsPerAgent();
 
-setTimeout(function(){console.log (getJobProperty(2, "agents"));}, 3000);
+//setTimeout(function(){console.log (getJobProperty(2, "agents"));}, 3000);
 
-setTimeout(function() {console.log(enumerateAgents());}, 5000);
+//setTimeout(function() {console.log(enumerateAgents());}, 5000);
 
-console.log(findArrayDiff([1, 5, 17, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160], [1, 5, 17, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 165]));
+//console.log(findArrayDiff([1, 5, 17, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160], [1, 5, 17, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 165]));
 
-var prevList, updatedList;
+var prevList;
+var updatedList = [];
 
 function periodicAgentUpdate() {
-    updatedList = enumerateAgents();
     prevList = updatedList; 
+    updatedList = enumerateAgents();
 
     if (findArrayDiff(updatedList, prevList).length != 0) {
         console.log("ALERT: " + findArrayDiff(updatedList, prevList));
