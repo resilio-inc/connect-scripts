728555570f8cfc7498d239827c76eb71c7843420
Ilan Shamir <ilan@resilio.com>
2020-08-21 17:53:54 -0700

    return promises for async calls and added support for storage deletion

--- Resilio Connect API/JSMonitor/storages.js
+++ Resilio Connect API/JSMonitor/storages.js
@@ -2,28 +2,41 @@
 
 module.exports = {
     addNewStorage,
+    deleteStorage,
 };
 
-const { initializeMCParams, getAPIRequest, postAPIRequest } = require('./communication');
+const { initializeMCParams, getAPIRequest, postAPIRequest, deleteAPIRequest } = require('./communication');
 
 function addNewStorage(type, name, desc, access_id, access_secret, bucket, region) {
+    var addNewStorageResponse = (resolve, reject) => {
+        const storageInfo = 
+        {
+            "type": type,
+            "name": name,
+            "description": desc,
+            "config": {
+                "access_id": access_id,
+                "access_secret": access_secret,
+                "bucket": bucket,
+                "region": region
+            }
+        };
 
-    const storageInfo = 
-    {
-        "type": type,
-        "name": name,
-        "description": desc,
-        "config": {
-            "access_id": access_id,
-            "access_secret": access_secret,
-            "bucket": bucket,
-            "region": region
-        }
-    };
-
-    postAPIRequest("/api/v2/storages", storageInfo)
-    .then((APIResponse) => {
-        console.log("MC Response: " + APIResponse);
-    });
-  
+        postAPIRequest("/api/v2/storages", storageInfo)
+        .then((APIResponse) => {
+            resolve(APIResponse);
+        });
+    }
+    return new Promise(addNewStorageResponse);  
 }
+
+function deleteStorage(storageID) {
+    var deleteStorageResponse = (resolve, reject) => {
+        deleteAPIRequest("/api/v2/storages/" + storageID)
+        .then((APIResponse) => {
+            resolve(APIResponse);
+        });
+    }
+    return new Promise(deleteStorageResponse);  
+}
+
