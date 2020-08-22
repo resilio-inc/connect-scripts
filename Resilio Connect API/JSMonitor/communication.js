5a4b18d5ec5b89c142fe2b21795eb41b74563a19
Ilan Shamir <ilan@resilio.com>
2020-08-21 17:52:11 -0700

    added support for POST and DELETE requests

--- Resilio Connect API/JSMonitor/communication.js
+++ Resilio Connect API/JSMonitor/communication.js
@@ -4,6 +4,7 @@ module.exports = {
     initializeMCParams,
     getAPIRequest,
     postAPIRequest,
+    deleteAPIRequest,
 };
 
 var https = require('https');
@@ -19,74 +20,85 @@ var mcToken = "";
  * @param {string} token 
  */
 function initializeMCParams(url, port, token) {
-    mcURL = url;
-    mcPort = port;
-    mcToken = "Token " + token;
+  mcURL = url;
+  mcPort = port;
+  mcToken = "Token " + token;
 }
 
 function getAPIRequest(APIReq) {
-    var APIResponse = (resolve, reject) => {
-        var options = {
-            headers: {"Authorization": mcToken},
-            host: mcURL,
-            port: mcPort,
-            path: APIReq
-        }
-    
-        https.get(options, function(res) {
-          var data = '';
-    
-          res.on('data', function(chunk) {
-            data += chunk;
-          });
-    
-          res.on('end', () => {
-            resolve(data); 
-    
-          }).on('error', (err) => {
-            console.log("there was an error:" + err);
-            reject(err);
-          }); 
-          
-        });
-      }
-      return new Promise(APIResponse)
+  var APIResponse = (resolve, reject) => {
+    var options = {
+      headers: { "Authorization": mcToken },
+      host: mcURL,
+      port: mcPort,
+      path: APIReq
+    }
+
+    https.get(options, function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(data);
+
+      }).on('error', (err) => {
+        console.log("there was an error:" + err);
+        reject(err);
+      });
+
+    });
+  }
+  return new Promise(APIResponse)
 }
 
+
+function APIRequest(APIReq, method, bodyData) {
+  var APIResponse = (resolve, reject) => {
+    if (bodyData.length != "") {
+      bodyData = JSON.stringify(bodyData);
+    }
+    var options = {
+      headers: {
+        "Authorization": mcToken,
+        "Content-Type": "application/json",
+        "Content-Length": bodyData.length
+      },
+      host: mcURL,
+      port: mcPort,
+      path: APIReq,
+      method: method,
+    }
+
+    const req = https.request(options, function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(data);
+
+      }).on('error', (err) => {
+        console.log("there was an error:" + err);
+        reject(err);
+      });
+
+    });
+    req.write(bodyData);
+    req.end();
+  }
+  return new Promise(APIResponse)
+}
 
 function postAPIRequest(APIReq, postData) {
-  var APIResponse = (resolve, reject) => {
-    postData = JSON.stringify(postData);
-      var options = {
-          headers: {
-            "Authorization": mcToken,
-            "Content-Type": "application/json",
-            "Content-Length": postData.length
-          },
-          host: mcURL,
-          port: mcPort,
-          path: APIReq,
-          method: "POST"
-      }
-  
-      const req = https.request(options, function(res) {
-        var data = '';
-  
-        res.on('data', function(chunk) {
-          data += chunk;
-        });
-  
-        res.on('end', () => {
-          resolve(data); 
-  
-        }).on('error', (err) => {
-          console.log("there was an error:" + err);
-          reject(err);
-        }); 
-        
-      });
-      req.write(postData);
-      req.end();
-    }
-    return new Promise(APIResponse)
+  return APIRequest(APIReq, "POST", postData);
 }
+
+function deleteAPIRequest(APIReq) {
+  return APIRequest(APIReq, "DELETE", "");
+}
+
