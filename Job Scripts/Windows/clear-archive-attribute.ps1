eb8e770f37f757a6aff790ab3550d4e880d3f691
Ilan Shamir <ilan@resilio.com>
2020-10-13 23:34:58 -0700

    much faster version of the script

--- Job Scripts/Windows/clear-archive-attribute.ps1
+++ Job Scripts/Windows/clear-archive-attribute.ps1
@@ -1,29 +1,32 @@
+$startTime = (Get-Date)
+"Script started"
+
 # current directory and any sub-directories
 $path = ".\"
+
+$attribute = [io.fileattributes]::archive
 
 # look at files that were modified more than 48 hours ago
 $cutoffDate = (get-date).addHours(-48).date
 $files = Get-ChildItem -Path $path -Recurse | `
 		 	Where-Object {!($_.psIsContainer) -AND `
-		 	($_.lastwritetime -lt $cutoffDate -AND $_.creationtime -lt $cutoffDate)}
+		 	($_.lastwritetime -lt $cutoffDate -AND $_.creationtime -lt $cutoffDate) -AND `
+			($_.attributes -band $attribute)}
 
-$attribute = [io.fileattributes]::archive
+$files_processed = 0
 
-"Script started"
 Foreach($file in $files) 
 {
-	If((Get-ItemProperty -Path $file.fullname).attributes -band $attribute)
-	{ 
-		"Removing Archive bit from '$file'"
-		Set-ItemProperty -Path $file.fullname -Name attributes `
-			-Value ((Get-ItemProperty $file.fullname).attributes -BXOR $attribute)
-		#"New value of '$file' attributes"
-		#(Get-ItemProperty -Path $file.fullname).attributes
-	}
-	ELSE
-	{ 
-		#Write-host -ForegroundColor blue `
-		#"'$file' does not have the $attribute bit set"
-	}
+	#"Removing Archive bit from '$file'"
+	$files_processed++
+	Set-ItemProperty -Path $file.fullname -Name attributes `
+		-Value ((Get-ItemProperty $file.fullname).attributes -BXOR $attribute)
+	#"New value of '$file' attributes"
+	#(Get-ItemProperty -Path $file.fullname).attributes
 }
+
+$endTime = (Get-Date)
+
+"Processed '$files_processed' files."
+"Processing took '$($endTime - $startTime)'"
 "Script done"
